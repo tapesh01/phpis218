054fc961316d75a32ef99765a6f890e0287fe38d
Tapesh Nagarwal <tn64@afsconnect2.njit.edu>
2016-09-29 14:27:18 -0400

    learned a little php and copied teacher's code

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
 <?php
 
   echo 'Hello World';
+  '<br>'
+
+ 
+  $myarray = array();
+  $myarray = 'some value 1';
+  $myarray = 'some value 2';
+  $myarray = 'some value 3';
+
+  print_r($myarray);
+
+  $myAssoc = array('value1' => $myarray, 'value2' => $myarray);
+
+  print_r($myAssoc);
+  
+  print_r($myAssoc['value1']);
+
+  class myclass {
+  
+  public $myPublic;
+  private $myPrivate;
+  protected $myProtected;
+
+  public function_construct() {
+  	
+	$this->myPublic = 1;
+	$this->myPrivate = 2;
+	$this->myProtected = 3;
+
+  }
+}	
+  $obj = new myclass;
+
+  print_r($obj);
+
+
+
 
 ?>
